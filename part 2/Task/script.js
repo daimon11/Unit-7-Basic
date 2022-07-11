de018bc46dc28c2a22ce5f7a50584be1007f1443
Dmitriy Bezgodko <daimon.94@mail.ru>
2022-07-11 16:38:29 +0300

    11-07-22(16-40)

--- part 2/Task/script.js
+++ part 2/Task/script.js
@@ -1,27 +1,37 @@
-const randomNum = Math.round(Math.random() * 100);
+let randomNum = parseInt(Math.random() * 100);
+console.log(randomNum);
 
-let userNumber = prompt('Привет! Я загадал число от 1 до 100, попробуй отгадать:', '');
-
-const guessNumber = (randomNum) => {
-  for (i = 0; i < randomNum; i++) {
-    if (!Number.isNaN(userNumber) && userNumber > 0 && userNumber !== null) {
-      if (userNumber > randomNum) {
-        userNumber = prompt('Многовато, попробуй меньше:', '');
-        i++;
-      } else if (userNumber < randomNum) {
-        userNumber = prompt('Маловато, попробуй больше:', '');
-        i++;
-      } else {
-        alert(`Верно, это число ${randomNum}!`);
-        break;
-      }
-    } else if (userNumber === null) {
+const isNum = (num) => {
+  switch (true) {
+    case (num === null):
+      alert('Пока-Пока!');
       break;
-    } else {
-      userNumber = prompt('Введи число!', '');
-      i++
-    }
+    case (isNaN(num) || num === ''):
+      alert('Вы ввели неккореткное значение!');
+      guessNumber();
+    default:
+      return !isNaN(parseFloat(num));
   }
 }
-guessNumber(randomNum);
 
+const guessNumber = () => {
+  let userNum = prompt('Привет! Я загадал число от 1 до 100. Попробуйте отгадать:');
+  if (isNum(userNum)) {
+    userNum = +userNum;
+    switch (true) {
+      case (userNum > randomNum):
+        alert('Вы ввели большее число');
+        return guessNumber();
+      case (userNum < randomNum):
+        alert('Вы ввели меньшее число');
+        return guessNumber();
+      case (userNum === randomNum):
+        alert('Угадал!');
+        break;
+    }
+  } else {
+    return userNum;
+  }
+}
+
+guessNumber();
